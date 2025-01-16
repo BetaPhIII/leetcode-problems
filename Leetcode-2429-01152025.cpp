064383a6f8e587e34f2e6dbb3a5feb5aa7bb95fa
Bryon Catlin <bryon.catlin@gmail.com>
2025-01-16 18:34:23 -0500

    accidentally uploaded the wrong leetcode for 2429 all stats from prev commit are still accurate :)

--- Leetcode-2429-01152025.cpp
+++ Leetcode-2429-01152025.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-        int bits1 = 0, copy1 = num1, bits2 = 0, copy2 = num2;
-        while (copy1 > 0) {
-            if (copy1 & 1) bits1++;
-            copy1 >>= 1;
-        }
-        while (copy2 > 0) {
-            if (copy2 & 1) bits2++;
-            copy2 >>= 1;
-        }
-
-        if (bits1 == bits2) return num1;
-        else if (bits1 > bits2) {
-            int ans = 0, bitcount = 0, copy1 = num1, temp = 1;
-            while (copy1 > 0) {
-                if (copy1 & 1) {
-                    bitcount++;
-                    if (bitcount > bits1 - bits2) ans += temp;
-                }
-                copy1 >>= 1;
-                temp <<= 1;
+        int set_x = countBits(num2);
+        int x = 0;
+        for(int i = (sizeof(num1)*8)-1; (i >= 0 && set_x>0); i--){
+            unsigned int mask = 1 << i;
+            if(num1 & mask){
+                x |= mask;
+                set_x--;
             }
-            return ans;
         }
-        else {
-            int ans = num1, bitcount = 0, temp = 1;
-            while (bitcount < bits2 - bits1) {
-                if (!(ans & temp)) {
-                    ans += temp;
-                    bitcount++;
-                }
-                temp <<= 1;
+        for(int i = 0;(set_x>0) && (i<(sizeof(num1)*8)-1);i++){
+            unsigned int mask = 1 << i;
+            if(!(x & mask)){
+                x |= mask;
+                set_x--;
             }
-            return ans;
         }
+        
+        return x;
+    }
+    int countBits(int num){
+        int count = 0;
+        unsigned int bits = sizeof(num)*8;
+        for (int i = bits - 1; i >= 0; i--) {
+            int mask = 1 << i;
+            if(num & mask){count++;}
+        }
+        return count;
     }
 };
